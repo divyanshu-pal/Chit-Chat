1071f43bc4e2a9faf5a622ff8a804e18951d2289
Divyanshu Pal <86774540+divyanshu-pal@users.noreply.github.com>
2024-05-21 11:31:39 +0530

    Add files via upload

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -105,7 +105,18 @@ const login = async (req, res, next) => {
     }
   };
 
+
+ const logOut = (req, res, next) => {
+    try {
+      if (!req.params.id) return res.json({ msg: "User id is required " });
+      onlineUsers.delete(req.params.id);
+      return res.status(200).send();
+    } catch (ex) {
+      next(ex);
+    }
+  };
+  
 module.exports={
-    register,login,setAvatar,getAllUsers
+    register,login,setAvatar,getAllUsers,logOut
 }
 
